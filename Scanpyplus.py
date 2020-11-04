82478d5d50cad5320479eeb75536680259b844eb
Ubuntu <brianpenghe@gmail.com>
2020-11-04 22:03:31 +0000

    now save gene names in logistic regression function

--- Scanpyplus.py
+++ Scanpyplus.py
@@ -491,6 +491,6 @@ def LogisticRegressionCellType(Reference, Query, Category = 'louvain', DoValidat
         scores = cross_val_score(logit, X, y, cv=cv)
 
     _ = joblib.dump(result,str(today)+'Sklearn.result.joblib.pkl',compress=9)
-
+    np.savetxt(str(today)+'Sklearn.result.csv',IntersectGenes,fmt='%s',delimiter=',')
     Query.obs['Predicted'] = y_predict
     return Query
